5c77506183343717ce9e07056cab331ae8d7cac0
dreamfox <dreamfox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-11 01:19:36 +0000

    version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-product-payments/tags/1.0.6@836486 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Plugin Name: Woocommerce Product Payments
+ * Plugin URI: www.dreamfox.nl 
+ * Version: 1.0.6
+ * Author: Marco van Loghum
+ * Author URI: www.dreamfox.nl 
+ * Description: Extend Woocommerce plugin to add payments methods to a product
+ * Requires at least: 3.7
+ * Tested up to: 3.5
+ */
+require_once ABSPATH . WPINC . '/pluggable.php';;
+require_once dirname(dirname(__FILE__)).'/woocommerce/classes/class-wc-payment-gateways.php';
+require_once dirname(dirname(__FILE__)).'/woocommerce/classes/class-wc-cart.php';
+
+add_action( 'add_meta_boxes', 'wpp_meta_box_add' );  
+function wpp_meta_box_add()  
+{  
+    add_meta_box( 'payments', 'Payments', 'wpp_payments_form', 'product', 'side', 'core' ); 
+}
+
+function wpp_payments_form()  
+{
+	global $post, $woo;
+	
+	$productIds = get_option('woocommerce_product_apply');
+	if( is_array( $productIds ) ){
+		foreach($productIds as $key=>$product){
+			if(!wp_get_single_post($product) || !count(get_post_meta($product, 'payments', true)) ){
+				unset($productIds[$key]);
+			}
+		}
+	}
+	update_option('woocommerce_product_apply', $productIds);
+	
+	$postPayments = count ( get_post_meta($post->ID, 'payments', true) ) ? get_post_meta($post->ID, 'payments', true) : array() ;
+	if( count( $productIds )>=10 && !count( $postPayments ) ){
+		echo 'Limit reached Please download full version package at www.dreamfox.nl!';
+		return;
+	}
+	
+	$woo = new WC_Payment_Gateways();
+	$payments = $woo->get_available_payment_gateways();
+	foreach($payments as $pay){
+		$checked = '';
+		if( is_array( $postPayments ) && in_array($pay->id, $postPayments)) $checked = ' checked="yes" '; ?>  
+		<input type="checkbox" <?php echo $checked; ?> value="<?php echo $pay->id; ?>" name="pays[]" id="payment_<?php echo $pay->id; ?>" />
+		<label for="payment_<?php echo $pay->id; ?>"><?php echo $pay->title; ?></label>  
+		<br />  
+		<?php
+	}      
+} 
+
+add_action('save_post', 'wpp_meta_box_save', 10, 2 );
+function wpp_meta_box_save( $post_id )  
+{   
+	if($_POST['post_type']=='product'){
+	
+		$productIds = get_option('woocommerce_product_apply');
+		if( !in_array( $post_id, $productIds ) && count( $productIds ) <= 10 ){
+			$productIds[] = $post_id;
+			update_option('woocommerce_product_apply', $productIds);
+		}
+	
+		//delete_post_meta($post_id, 'payments');	 
+		$payments = array();
+		if($_POST['pays']){
+			foreach($_POST['pays'] as $pay)
+				$payments[] = $pay;
+		}		
+		update_post_meta($post_id, 'payments', $payments); 
+	}
+}
+
+function wpp_payment_gateway_disable_country( $available_gateways ) {
+	global $woocommerce;
+	
+	$arrayKeys = array_keys($available_gateways);
+	$items = $woocommerce->cart->cart_contents;
+	$itemsPays = '';
+	if($items){
+		foreach($items as $item){
+			$itemsPays = get_post_meta($item['product_id'], 'payments', true) ;
+			if( count($itemsPays) ){
+				foreach( $arrayKeys as $key )
+					if( is_array( $itemsPays ) && !in_array( $key, $itemsPays ) )
+						unset($available_gateways[$key]);
+			}
+		}
+	}
+	return $available_gateways;
+}
+add_filter( 'woocommerce_available_payment_gateways', 'wpp_payment_gateway_disable_country' );
+?>
